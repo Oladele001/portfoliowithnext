41a74042bd32422a2b26f82abe5b079c55f35475
oladele001 <abdulrahmanqozeem15@gmail.com>
2025-11-19 13:30:19 +0100

    project section was added

--- app/Component/Project.jsx
+++ app/Component/Project.jsx
@@ -5,14 +5,50 @@ const Project = () => {
     <div className='h-fit w-full bg-[#212529] p-2 '>
         <p className='text-red-500 text-center text-2xl '>Visit My Projests And Keep your Feed Back</p>
         <h1 className='text-4xl text-white text-center font-bold'>My Projects</h1>
-        <div className='flex flex-col gap-3 pt-6'>
-            <div className='w-full h-96 rounded-xl bg-gray-800 border-2 border-red-300'> 
-                <div className=' w-full h-80 border-2 border-green-500 '></div>
+        <div className='grid grid-cols-1 gap-3 pt-6'>
+            <div className='w-full h-96 rounded-xl p-4 bg-gray-700'> 
+                <div className=' w-full h-64 rounded-2xl '>
+                    <img src="Ip address tracker.png" alt="" className='h-64 rounded-2xl ' />
+                </div>
+                <p className='text-red-500 text-xl mt-5 '>Ip Address Tracker</p>
+                <p className='text-white text-sm mt-3 '>This is ip address to track any adress using the ip number</p>
             </div>
-            <div className='w-full h-24 bg-gray-800 border-2 border-red-300'> </div>
-            <div className='w-full h-24 bg-gray-800 border-2 border-red-300'> </div>
-            <div className='w-full h-24 bg-gray-800 border-2 border-red-300'> </div>
-            <div className='w-full h-24 bg-gray-800 border-2 border-red-300'> </div>
+            <div className='w-full h-96 rounded-xl p-4 bg-gray-700'> 
+                <div className=' w-full h-64 rounded-2xl '>
+                    <img src="Ip address tracker.png" alt="" className='h-64 rounded-2xl ' />
+                </div>
+                <p className='text-red-500 text-xl mt-5 '>Ip Address Tracker</p>
+                <p className='text-white text-sm mt-3 '>This is ip address to track any adress using the ip number</p>
+            </div>
+            <div className='w-full h-96 rounded-xl p-4 bg-gray-700'> 
+                <div className=' w-full h-64 rounded-2xl '>
+                    <img src="Ip address tracker.png" alt="" className='h-64 rounded-2xl ' />
+                </div>
+                <p className='text-red-500 text-xl mt-5 '>Ip Address Tracker</p>
+                <p className='text-white text-sm mt-3 '>This is ip address to track any adress using the ip number</p>
+            </div>
+            <div className='w-full h-96 rounded-xl p-4 bg-gray-700'> 
+                <div className=' w-full h-64 rounded-2xl '>
+                    <img src="Ip address tracker.png" alt="" className='h-64 rounded-2xl ' />
+                </div>
+                <p className='text-red-500 text-xl mt-5 '>Ip Address Tracker</p>
+                <p className='text-white text-sm mt-3 '>This is ip address to track any adress using the ip number</p>
+            </div>
+            <div className='w-full h-96 rounded-xl p-4 bg-gray-700'> 
+                <div className=' w-full h-64 rounded-2xl '>
+                    <img src="Ip address tracker.png" alt="" className='h-64 rounded-2xl ' />
+                </div>
+                <p className='text-red-500 text-xl mt-5 '>Ip Address Tracker</p>
+                <p className='text-white text-sm mt-3 '>This is ip address to track any adress using the ip number</p>
+            </div>
+            <div className='w-full h-96 rounded-xl p-4 bg-gray-700'> 
+                <div className=' w-full h-64 rounded-2xl '>
+                    <img src="Ip address tracker.png" alt="" className='h-64 rounded-2xl ' />
+                </div>
+                <p className='text-red-500 text-xl mt-5 '>Ip Address Tracker</p>
+                <p className='text-white text-sm mt-3 '>This is ip address to track any adress using the ip number</p>
+            </div>
+            
         </div>
     </div>
   )
